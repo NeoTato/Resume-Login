3669a60d7e1093793521623f0d3604068ef2bae7
Eon_Busque <23-00175@g.batstateu.edu.ph>
2025-09-27 20:37:26 +0800

    fixed href for css in resume.php

--- public/resume.php
+++ public/resume.php
@@ -41,7 +41,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/resumeStyles.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/resumeStyles.css" type="text/css">
     <title>My Resume</title>
 </head>
 
